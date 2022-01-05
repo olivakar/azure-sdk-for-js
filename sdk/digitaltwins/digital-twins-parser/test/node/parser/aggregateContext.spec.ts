6779a7b3d2f28e6da331e377d798dec3feb1428f
Yoseph Maguire <yoseph.maguire@gmail.com>
2022-01-05 08:34:25 -0800

    remove tests

--- sdk/digitaltwins/digital-twins-parser/test/node/parser/aggregateContext.spec.ts
+++ sdk/digitaltwins/digital-twins-parser/test/node/parser/aggregateContext.spec.ts
@@ -2,7 +2,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import {AggregateContext} from '../../../src/generated';
+import {AggregateContext} from '../../../src/parser';
 import {ParsingError} from '../../../src/parser/parsingError';
 import {expect} from 'chai';
 
